dacda9045d5330634fcdc2a8a8c1ef2918f4f9fa
techminds <aztechminds@gmail.com>
2025-01-16 12:28:02 +0100

    modified with Helen in class

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(express.json());
 
 // Array für die Notizen
 let notes = [
-  { note: "My new Note", autor: "Max Mustermann", date: "2025-01-15" },
+  { id: 1, note: "My new Note", autor: "Max Mustermann", date: "2025-01-15" },
 ];
 
 app.get("/", (request, response) => {
@@ -20,61 +20,56 @@ app.get("/notes", (req, res) => {
 
 // GET /notes/:id - Notiz nach ID abrufen
 app.get("/notes/:id", (req, res) => {
-  let id = parseInt(req.params.id);
+  const id = parseInt(req.params.id);
+  let findNote = null;
+  notes.forEach((note) => {
+    if (note.id === id) {
+      findNote = note;
+    }
+  });
 
-  if (isNaN(id) || id <= 0) {
-    return res.status(404).send("ID Not Found");
-  }
-
-  id -= 1;
-
-  if (id in notes) {
-    return res.json(notes[id]);
-  } else {
-    return res.status(404).send("ID Not Found");
-  }
+  // const note = notes.find((note) => note.id === id);
+  res.json(findNote);
 });
 
 // POST /notes - Neue Notiz erstellen
 app.post("/notes", (req, res) => {
-  console.log(req.body);
-  let note = req.body;
-  notes.push(note);
+  const lastId = notes[notes.length - 1].id;
+  console.log(lastId);
+  const newNote = {
+    id: lastId + 1,
+    note: req.body.note,
+    autor: req.body.autor,
+    date: new Date(),
+  };
+  notes.push(newNote);
   res.send("Note has been stored");
 });
 
 // PUT /notes/:id - Notiz aktualisieren
 app.put("/notes/:id", (req, res) => {
   let id = parseInt(req.params.id);
-  let note = req.body;
-  if (isNaN(id) || id <= 0) {
-    res.status(404).send("ID Not Found");
-  }
-  id -= 1;
+  let updateNote = null;
+  notes.forEach((note) => {
+    if (note.id === id) {
+      updateNote = note;
+    }
+  });
 
-  if (id in notes) {
-    notes[id] = note;
-    res.send("Note has been updated");
-  } else {
-    res.status(404).send("ID Not Found");
+  if (updateNote !== null) {
+    updateNote.note = req.body.note;
+    updateNote.autor = req.body.autor;
+    updateNote.date = new Date();
   }
+  res.send(updateNote);
 });
 
 // DELETE /notes/:id - Notiz löschen
 app.delete("/notes/:id", (req, res) => {
   let id = parseInt(req.params.id);
-  let note = req.body;
-  if (isNaN(id) || id <= 0) {
-    return res.status(404).send("ID Not Found");
-  }
-  id -= 1;
-
-  if (id in notes) {
-    delete notes[id];
-    return res.send("Note has been deleted");
-  } else {
-    return res.status(404).send("ID Not Found");
-  }
+  notes = notes.filter((note) => note.id !== id);
+  console.log(notes);
+  res.send(notes);
 });
 
 // Server starten
